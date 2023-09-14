8724ea063e7139b950610c73ff573047caf6fe8f
Dre <developerdrequanwalker@gmail.com>
2023-09-14 19:38:06 -0400

    Assessment

--- geometric_shape_area_calculator.py
+++ geometric_shape_area_calculator.py
@@ -6,27 +6,40 @@ def main():
     circle_pi = math.pi # DO NOT MODIFY, this line of code is assigning the variable 'circle_pi' equal to Pi ~3.14
 
     # TODO: In terminal, print Welcome to the geometric shape area calculator!
+    print('Welcome to the geometric area calculator')
     
     # User Options
-    # Circle = 1
-    # Rectangle = 2
-    # Triangle = 3
+    Circle = 1
+    Rectangle = 2
+    Triangle = 3
+
+   
     
     # TODO: Using one print statement, use string concatenation to print the options only 
     # as a single string (make sure to add a space between each option)
+    print(Circle + " " + Rectangle + "" + Triangle)
+    
+    
 
-    # TODO: In terminal, ask the user "Select a shape by entering 1, 2, or 3' and assign the input to a new variable named 'choice'.
+    # TODO: In terminal, ask the user "Select a shape by entering 1, 2, or 3' and assign the input to a new variable named 'choice'
+    choice = input("Select a shape by entering 1, 2, 3")
 
     # TODO: Convert the variable 'choice' to an integer data type.
+    choice = int(choice)
 
     # TODO: With one line of code, verify the variable 'choice' is indeed the data type integer, use conditional logic and print the output.  If converted correctly, the output in Terminal should read 'True'.
+    if (choice, int):
+     print(True)
   
     if choice == 1:  #DO NOT REMOVE THE 'IF'
         # Calculate the area of a circle
 
         # TODO: Assign a new variable named 'radius' and ask for the user's input for the radius of the circle.
+       
         # TODO: Convert 'radius' to float.
+         radius = float(input("Enter the radius of the circle: ")) 
         # TODO: Assign a new variable named 'area' and implement the logic to calculate the area of a circle.
+         area = circle_pi * radius**2
         # HINT 1 : The formula to find area of a circle: 'circle_pi' times radius squared.  
         # Hint 2 : circle_pi is a variable that has been assigned on Line 9 and equals Pi in math.  
 
@@ -37,16 +50,30 @@ def main():
         # TODO: Assign a new variable 'area' and implement the logic to calculate the area of a rectangle.
         # HINT: The formula to find the area of a rectangle: length times width
 
+        length = float(input("Enter the length of the rectangle: "))
+        width = float(input("Enter the width of the rectangle: "))
+
+        area = length * width
+
+        print(f"The area of the rectangle is: {area:.2f} square units.")
+        
     elif choice == 3: #DO NOT REMOVE THE 'ELIF'
         # Calculate the area of a triangle
         # TODO: Assign new variables 'base' and 'height' and ask for the user's input for the base length and height of the triangle.
         # TODO: Convert both 'base' and 'height' to float.
         # TODO: Assign a new variable 'area' and implement the logic to calculate the area of a triangle.
         # HINT: The formula to find the area of a Triangle: half times base times height
+        base = float(input("Enter the base of the triangle: "))
+        height = float(input("Enter the height of the triangle: "))
+
+        area = 0.5 * base * height
+
+        
 
     else:
         # TODO: If the user enters anything other than 1, 2 or 3, print statement "Invalid choice ."
-    
+     print("Invalid choice")
+
     if choice in [1, 2, 3]: # DO NOT MODIFY
         print(f"The area is: {area:.2f} square units.") # DO NOT MODIFY
 
